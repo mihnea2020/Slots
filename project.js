87a71faa8d778878edeffafe5e8d4396d17f4c55
Mihnea Iancu <mihnea2846@gmail.com>
2023-09-19 22:10:57 +0200

    deposit money

---
+++ project.js
@@ -0,0 +1,15 @@
+// 1. Deposit some money
+// 2. Determine the number of lines to bet
+// 3. Collect a bet ammount
+// 4. Spin the slot machine
+// 5. Check if the user won
+// 6. Give the user their winnings
+// 7. Play again
+
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+    const depositAmount = prompt("Enter a deposit amount: ");
+};
+
+deposit();
