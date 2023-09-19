56b0084d4d28c53ff4885542bdff32fd74efb420
Mihnea Iancu <mihnea2846@gmail.com>
2023-09-19 23:11:23 +0200

    spin is done

--- project.js
+++ project.js
@@ -69,12 +69,26 @@ const getBet = (balance, lines) => {
 
 const spin = () => {
     const symbols = [];
-    for(const[symbol, count] of Object.entries(SYMBOLS_COUNT)) {
-        console.log(symbol, count);
+    for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+        for (let i = 0; i < count; i++) {
+            symbols.push(symbol);
+        }
     }
+    const reels = [[], [], []];
+    for (let i = 0; i < COLS; i++) {
+        const reelSymbols = [...symbols];
+        for (let j = 0; j < ROWS; j++) {
+            const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+            const selectedSymbol = reelSymbols[randomIndex];
+            reels[i].push(selectedSymbol);
+            reelSymbols.splice(randomIndex, 1);
+        }
+    }
+    return reels;
 }
 
-spin();
+const reels = spin();
+console.log(reels);
 
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
